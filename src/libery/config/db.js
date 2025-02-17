2c4126401a9c734090b99200d2d283af60aba00b
Md Sumon Rari <sumon784969@gmail.com>
2025-02-17 08:00:24 +0600

    envfile using

--- src/libery/config/db.js
+++ src/libery/config/db.js
@@ -4,6 +4,7 @@ DB_PASS:FNAX7covgcpeFbvD
 */
 import mongoose from "mongoose";
 export const connectDB=async ()=>{
-    await mongoose.connect(`mongodb+srv://hasimAmla:FNAX7covgcpeFbvD@cluster0.ihmonoj.mongodb.net/blog-app`)
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ihmonoj.mongodb.net/blog-app`)
     console.log("DB CONNECT")
+   
 }
